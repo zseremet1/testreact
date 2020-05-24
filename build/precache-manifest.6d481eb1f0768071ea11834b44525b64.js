680f7d8bd2803dc6176ae5bfd4ab7e60908140f5
MukundhBhushan <aknsmb@gmail.com>
2020-05-24 21:39:56 +0530

    init commit

---
+++ build/precache-manifest.6d481eb1f0768071ea11834b44525b64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8c8e51149f3c12af02a7d98f351277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec4ad442b0790d2fe146",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c050987d7a89ac9b10f",
+    "url": "/static/js/2.f4b57ed2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4b57ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4ad442b0790d2fe146",
+    "url": "/static/js/main.94dda156.chunk.js"
+  },
+  {
+    "revision": "1236f79c1da58474fd54",
+    "url": "/static/js/runtime-main.1856a2c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
